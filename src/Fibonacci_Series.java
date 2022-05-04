d92b484470344de8eeb41b065cd553a49f3ec25c
SSK137 <saurabhkodam@gmail.com>
2022-05-04 09:41:42 +0530

    Logical Programs : Fibonacci Series

---
+++ src/Fibonacci_Series.java
@@ -0,0 +1,23 @@
+public class Fibonacci_Series {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Logical Programming");
+        // veriable declaration
+        int FirstNumber=0;
+        int SecondNumber=1;
+        int sum=0;
+        /*In Every Fibonacci series it start with 0 and 1 so
+        we print them first directly
+         */
+        System.out.println(FirstNumber);
+        System.out.println(SecondNumber);
+        //Calculating Fibonacci Series
+        for(int i=FirstNumber;i<=5;i++){
+            for(int j=SecondNumber;j<=5;j++){
+                sum=FirstNumber+SecondNumber;
+                FirstNumber=SecondNumber;
+                SecondNumber=sum;
+                System.out.println(sum);
+            }
+        }
+    }
+}
